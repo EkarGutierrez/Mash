b3c6748d56b8b8c0f1c968f6159c0a327789e59d
Dayber Alfonso <0036161sm@gmail.com>
2024-04-10 17:19:05 -0400

    Create server.js

---
+++ Mash/BackEnd/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://Dayber:Mashdatabase@mashdatabase.2o2fmji.mongodb.net/?retryWrites=true&w=majority&appName=MashDatabase", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+// Routes
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
